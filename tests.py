fd9ea9e8f04b478b48bd853920709886f5241882
carlaIosim <carla.iosim03@e-uvt.ro>
2026-01-22 11:00:13 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,14 +1,25 @@
-#import all needed modules here
+# tests.py
 import unittest
+# We import the function we want to test from app.py
+from app import interpret_score
 
-#write all your tests below this line
+class TestWorkEngagement(unittest.TestCase):
 
+    def test_interpret_high(self):
+        # Requirement: High if mean > 4
+        self.assertEqual(interpret_score(4.5), "High")
+        self.assertEqual(interpret_score(6), "High")
 
-#write your test suite here, in the main() function
-def main():
-    #call all your tets here, one on each line
-    print("Starting tests suite...")
-    
-#please do not change the lines below
-if __name__ == "__main__":
-    main()
+    def test_interpret_moderate(self):
+        # Requirement: Moderate if 2 <= mean <= 4
+        self.assertEqual(interpret_score(4.0), "Moderate")
+        self.assertEqual(interpret_score(2.0), "Moderate")
+        self.assertEqual(interpret_score(3.5), "Moderate")
+
+    def test_interpret_low(self):
+        # Requirement: Low if mean < 2
+        self.assertEqual(interpret_score(1.5), "Low")
+        self.assertEqual(interpret_score(0), "Low")
+
+if __name__ == '__main__':
+    unittest.main()
